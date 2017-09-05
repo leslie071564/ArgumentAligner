0c12c5e94fe00c9f2bb8cf454f72bf084d5f2270
Yin-Jou Huang <leslie071564@gmail.com>
2017-09-05 13:08:04 +0900

    add feature generation module

---
+++ FeatureGenerator.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+import sys
+import shelve
+import random
+from utils import ALL_ALIGN
+
+class FeatureGenerator(object):
+    allFeats = {'Binary': ("get_binary", 'bin'), \
+                'PostPred': ("get_postPred", 'post'), \
+                'Conflict': ("get_conflict", 'conf'), \
+                'Cfsim': ("get_cfsim", 'cfsim'), \
+                'Core': ("get_core", 'core'), \
+                }
+
+    def __init__(self, config):
+        self.feat_db = config.feat_db
+
+        self.normalFeats = config.normalFeats
+        self.rivalFeats = config.rivalFeats
+        self.combFeats = config.combFeats
+
+        self.neg_sample_size = config.neg_sample_size
+    
+    def get_test_features(self, ev_id, only_gold=False):
+        goldRaw, goldSets, feat_dict = self._get_eventchain_data(ev_id)
+
+        # begin of file & comment line. 
+        feature_strs = []
+        feature_strs.append("@boi")
+        feature_strs.append("# %s %s" % (ev_id, goldRaw))
+
+        #
+        allAlign = goldSets if only_gold else ALL_ALIGN
+        for cf_pair in feat_dict.keys():
+            if cf_pair == 'general':
+                continue
+            for alignment in allAlign:
+                if only_gold:
+                    gold_index = allAlign.index(alignment)
+                    classStr = self.get_classStr(ev_id, "%s_%s" % (cf_pair, gold_index), alignment)
+                else:
+                    classStr = self.get_classStr(ev_id, cf_pair, alignment)
+
+                testFeatStr = self.get_featStr(feat_dict, alignment, cf_pair)
+                feature_strs.append("-%s %s" % (classStr, testFeatStr))
+
+        # end of file
+        feature_strs.append("@eoi\n")
+        return feature_strs
+
+    def get_train_features(self, key):
+        ev_id, cf1, cf2, gold_num = key.split('_')
+        cf_pair = "%s_%s" % (cf1, cf2)
+        goldRaw, goldSets, feat_dict = self._get_eventchain_data(ev_id)
+        goldAlign = goldSets[int(gold_num)]
+
+        # begin of file & comment line. 
+        feature_strs = []
+        feature_strs.append("@boi")
+        feature_strs.append("# %s %s" % (ev_id, goldRaw))
+
+        # print positive instance.
+        classStr = self.get_classStr(ev_id, cf_pair, goldAlign)
+        posFeatStr = self.get_featStr(feat_dict, goldAlign, cf_pair)
+        feature_strs.append("+%s %s" % (classStr, posFeatStr))
+
+        # print negative instances.
+        for alignment in random.sample(ALL_ALIGN, self.neg_sample_size):
+            if alignment == goldAlign:
+                continue
+
+            classStr = self.get_classStr(ev_id, cf_pair, alignment)
+            negFeatStr = self.get_featStr(feat_dict, alignment, cf_pair)
+            feature_strs.append("-%s %s" % (classStr, negFeatStr))
+
+        # end of file
+        feature_strs.append("@eoi\n")
+
+        return feature_strs
+
+    def _get_eventchain_data(self, ev_id):
+        data_dict = shelve.open(self.feat_db, flag='r')[ev_id]
+        goldRaw = " ".join(data_dict['goldRaw'])
+        goldSets = data_dict['goldSets']
+        feat_dict = data_dict['features']
+
+        return goldRaw, goldSets, feat_dict
+
+    def get_classStr(self, ev_id, cf_pair, alignment):
+        alignStr = "null" if alignment == [] else "_".join(alignment)
+        classStr = "%s_%s_%s" % (ev_id, cf_pair, alignStr)
+        return classStr
+
+    def get_featStr(self, feat_dict, align, cf_pair):
+        allFeats = []
+
+        for feat in self.normalFeats:
+            if feat not in FeatureGenerator.allFeats.keys():
+                continue
+            fx, postfix = FeatureGenerator.allFeats[feat]
+            allFeats.append(getattr(self, fx)(align, cf_pair, postfix, feat_dict))
+
+        for feat in self.rivalFeats:
+            rivType = feat.rstrip('Riv')
+            allFeats.append(self.get_rival(align, cf_pair, rivType, feat_dict))
+
+        allFeats = filter(None, allFeats)
+        return " ".join(allFeats)
+
+    ### general features.
+    def get_binary(self, align, cf_pair, postfix, feat_dict):
+        binaryFeats = []
+        for a in align:
+            binaryFeats.append("%s_%s" % (a, postfix))
+
+        return " ".join(binaryFeats) 
+
+    def get_postPred(self, align, cf_pair, postfix, feat_dict):
+        c2s = set(map(lambda x: x.split('-')[1], align))
+        posts = set(feat_dict['general']['postPred'])
+        postFeats = ["%s_%s" % (c2, postfix) for c2 in c2s & posts]
+
+        return " ".join(postFeats)
+
+    def get_conflict(self, align, cf_pair, postfix, feat_dict):
+        confFeats = []
+
+        conflictScores = feat_dict['general']['conflict']
+        for a in set(align) & set(conflictScores.keys()):
+            c1, c2 = a.split('-')
+            score = float(conflictScores[a]) / conflictScores["%s2" % (c2)]
+            confFeats.append("%s_%s:%.3f" % (a, postfix, score))
+
+        return " ".join(confFeats)
+
+    ### cf-related features.
+    def get_cfsim(self, align, cf_pair, postfix, feat_dict):
+        cfsimFeats = []
+        
+        cfsimScores = feat_dict[cf_pair]['cfsim']
+        for a, cfsim in cfsimScores.iteritems():
+            if '_' in a:    # items used for normalization.
+                continue
+            cfsim = cfsim if a in align else (-1) * cfsim
+            cfsimFeats.append("%s_%s:%.3f" % (a, postfix, cfsim))
+
+        return " ".join(cfsimFeats)
+
+    def get_core(self, align, cf_pair, postfix, feat_dict):
+        coreFeats = []
+
+        coreAligns = feat_dict[cf_pair]['core']
+        for a in set(align) & set(coreAligns):
+            coreFeats.append("%s_%s" % (a, postfix))
+
+        return " ".join(coreFeats)
+
+    ###
+    def get_rival(self, align, cf_pair, rivType, feat_dict):
+        # threshold could be added.
+        rivFeats = []
+        if rivType == 'Cfsim':
+            rivScores, postfix = feat_dict[cf_pair]['cfsim'], 'cfsimRiv'
+        else:
+            return ""
+
+        for a in set(align) & set(rivScores.keys()):
+            c1, c2 = a.split('-')
+            score = float(rivScores[a])
+            ratio1 = score / rivScores["%s-_" % c1]
+            ratio2 = score / rivScores["_-%s" % c2]
+            rivFeats.append("%s_%s:%.3f" % (a, postfix, ratio1 * ratio2))
+        return " ".join(rivFeats)
+
---
+++ print_feature_file.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import sys
+import yaml
+import argparse
+from argparse import Namespace
+from FeatureGenerator import FeatureGenerator
+import utils
+
+def setArgs(parser):
+    subparsers = parser.add_subparsers(dest="_subcommand")
+
+    test_parser = subparsers.add_parser("print_test")
+    test_parser.add_argument("--config_file", action="store", dest="config_file")
+    test_parser.add_argument('-i', "--id", action="store", dest="id")
+    test_parser.add_argument('-g', "--only_gold_align", action='store_true', dest="only_gold_align")
+    #test_parser.add_argument('-w', "--without_impossible_align", action='store_true', dest="without_impossible_align")
+    test_parser.add_argument('-o', "--output_file", action='store', dest='output_file')
+
+    train_parser = subparsers.add_parser("print_train")
+    train_parser.add_argument("--config_file", action="store", dest="config_file")
+    train_parser.add_argument('-k', "--key", action='store', dest='key')
+    train_parser.add_argument('-o', "--output_file", action='store', dest='output_file')
+
+    return parser
+
+def setPrintFeatureConfig(config_file):
+    config = yaml.load(open(config_file, 'r'))
+    feature_config = Namespace()
+
+    feature_config.feat_db = config['Output']['FEAT']
+
+    feature_config.normalFeats, feature_config.rivalFeats, feature_config.combFeats = [], [], []
+    for feat in config['Feature extraction']['Load']:
+        if feat.endswith('Riv'):
+            feature_config.rivalFeats.append(feat)
+        elif "*" in feat:
+            feature_config.combFeats.append(feat)
+        else:
+            feature_config.normalFeats.append(feat)
+
+    feature_config.neg_sample_size = config['Training']['NegSample']
+    return feature_config
+
+def print_train_feature_file(options):
+    feature_config = setPrintFeatureConfig(options.config_file)
+    printer = FeatureGenerator(feature_config)
+    feature_strs = printer.get_train_features(options.key)
+
+    output_file = open(options.output_file, 'w')
+    output_file.write('\n'.join(feature_strs))
+    output_file.close()
+
+def print_test_feature_file(options):
+    feature_config = setPrintFeatureConfig(options.config_file)
+    printer = FeatureGenerator(feature_config)
+    feature_strs = printer.get_test_features(options.id, only_gold=options.only_gold_align)
+
+    output_file = open(options.output_file, 'w')
+    output_file.write('\n'.join(feature_strs))
+    output_file.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    setArgs(parser)
+    options = parser.parse_args() 
+    
+    if options._subcommand == "print_train":
+        print_train_feature_file(options)
+
+    elif options._subcommand == "print_test":
+        print_test_feature_file(options)
+
