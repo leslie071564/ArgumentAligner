c682e8018b93adc647b6aca4180e012df1a9d9f0
Yin-Jou Huang <leslie071564@gmail.com>
2017-10-31 14:42:43 +0900

    max_sent_num option added to sid_to_sentence.py

--- sid_to_sentence.py
+++ sid_to_sentence.py
@@ -10,13 +10,14 @@ if __name__ == "__main__":
     parser.add_argument("--id", action="store", dest="id")
     parser.add_argument("--ev_db", action="store", dest="ev_db")
     parser.add_argument("--sid2sent", action="store", default="/pear/share/www-uniq/v2006-2015.text-cdb", dest="sid2sent")
+    parser.add_argument("--max_sent_num", action="store", default=50, type=int, dest="max_sent_num")
     options = parser.parse_args() 
    
     ev_db = shelve.open(options.ev_db, flag='r')
     ev_dict = ev_db[options.id]
 
     sents = []
-    for sid in ev_dict['supSents']:
+    for sid in ev_dict['supSents'][:options.max_sent_num]:
         sent = get_sentence_by_sid(sid, options.sid2sent)
         if sent:
             sents.append(sent)
