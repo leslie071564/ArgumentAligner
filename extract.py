a3db78290fac24005db76c22a19735ebe1669056
Yin-Jou Huang <leslie071564@gmail.com>
2017-11-11 20:12:46 +0900

    add write_config to extract module

--- extract.py
+++ extract.py
@@ -10,13 +10,18 @@ import utils
 import itertools
 from EventChain import EventChain
 
+import extract_config
+INPUT_VERS = ['gold', '170724', '170905']
+
 def setArgs(parser):
     subparsers = parser.add_subparsers(dest="_subcommand")
 
-    config_parser = subparsers.add_parser("print_config")
-    config_parser.add_argument("--template", action="store", dest="template")
+    config_parser = subparsers.add_parser("write_config")
+    config_parser.add_argument("input_ver", action="store", choices=INPUT_VERS)
+    config_parser.add_argument("extract_dir", action="store")
+    config_parser.add_argument("--config_file", action="store", dest="config_file")
+
     config_parser.add_argument("--arg_file", action="store", dest="arg_file")
-    config_parser.add_argument("--output_dir", action="store", dest="output_dir")
     config_parser.add_argument("--tmp_subdir", action="store", default="tmp", dest="tmp_subdir")
     config_parser.add_argument("--tmp_prefix", action="store", default="single.db", dest="tmp_prefix")
 
@@ -33,8 +38,26 @@ def setArgs(parser):
     merge_parser = subparsers.add_parser("merge_tmp_db")
     merge_parser.add_argument("--config_file", action="store", dest="config_file")
 
-def print_config(options):
-    pass
+def write_config(options):
+    input_paths = extract_config.INPUT_DEFS[options.input_ver]
+    input_dir = input_paths['input_dir']
+    for arg, path in input_paths.items():
+        if path != None and '$' in path:
+            path = path.replace('$input_dir', input_dir).replace('$extract_dir', options.extract_dir)
+            input_paths[arg] = path
+    if options.arg_file:
+        input_paths['arg_file'] = options.arg_file
+
+
+    db_paths = { arg: path.replace('$input_ver', options.input_ver) for arg, path in extract_config.EXTRACT_DBS.items() }
+    db_paths['KNP_SUB_DEPTH'] = 4 if options.input_ver == "gold" else 1
+
+    output_paths = { arg: path.replace('$extract_dir', options.extract_dir) for arg, path in extract_config.OUTPUT.items() }
+
+    config_dict = {'input_pas': input_paths, 'db': db_paths, 'output': output_paths}
+    with open(options.config_file, 'w') as f:
+        yaml.dump(config_dict, f, default_flow_style=False)
+    print options.config_file
 
 def print_task(options):
     config_file = options.config_file
@@ -54,21 +77,21 @@ def setEventChainConfig(config_file):
     config = yaml.load(open(config_file, 'r'))
 
     evs_config = Namespace()
-    config_raw = config['Raw']
-    evs_config.gold_file = config_raw['GOLD']
-    evs_config.word_replace_db = config_raw['WORD_REPLACE']
-    evs_config.key2sid = config_raw['KEY_SID']
-    evs_config.sid2pa = config_raw['SID_PA']
-    evs_config.sid2sent_dir = config_raw['SID_SENT_DIR']
+    config_raw = config['input_pas']
+    evs_config.gold_file = config_raw['gold_file']
+    evs_config.word_replace_db = config_raw['word_replace']
+    evs_config.key2sid = config_raw['key2sid']
+    evs_config.sid2pa = config_raw['sid2pa']
+    evs_config.sid2sent_dir = config_raw['sid2sent_dir']
 
-    db_locs = config['DB']
+    db_locs = config['db']
     evs_config.cf_cdb = db_locs['CF_CDB']
     evs_config.count_db = db_locs['COUNT_DB']
     evs_config.knp_index_db = db_locs['KNP_INDEX_DB']
     evs_config.knp_parent_dir = db_locs['KNP_PARENT_DIR']
-    evs_config.knp_sub_index_length = int(db_locs['KNP_SUB_LENGTH'])
+    evs_config.knp_sub_index_length = int(db_locs['KNP_SUB_DEPTH'])
 
-    evs_config.output_db = config['Output']['TMP_DB_PREFIX']
+    evs_config.output_db = config['output']['tmp_db_dir']
 
     return evs_config
 
@@ -78,14 +101,14 @@ def init_event_chain(options):
     evs.export()
 
 def merge_tmp_db(config_file):
-    config = yaml.load(open(config_file, 'r'))['Output']
+    config = yaml.load(open(config_file, 'r'))['output']
 
-    ev_db = shelve.open(config['EVENT_PAIR'])
-    feat_db = shelve.open(config['FEAT'])
-    ids_file = open(config['IDS'], 'wb')
+    ev_db = shelve.open(config['ev_db'])
+    feat_db = shelve.open(config['feat_db'])
+    ids_file = open(config['ids'], 'wb')
 
     doc_freq = defaultdict(int)
-    for f in utils.search_file_with_prefix(config['TMP_DB_PREFIX']):
+    for f in utils.search_file_with_prefix(config['tmp_db_dir']):
         ID = f.split('_')[-1]
         tmp_db = shelve.open(f, flag='r')
         
@@ -98,7 +121,7 @@ def merge_tmp_db(config_file):
     feat_db.close()
     ids_file.close()
 
-    set_tf_idf(config['EVENT_PAIR'], config['FEAT'])
+    set_tf_idf(config['ev_db'], config['feat_db'])
 
 def set_tf_idf(ev_db, feat_db):
     ev_db = shelve.open(ev_db)
@@ -156,6 +179,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     setArgs(parser)
     options = parser.parse_args()
+
+    if options._subcommand == "write_config":
+        write_config(options)
 
     if options._subcommand == "print_task":
         print_task(options)
--- extract.sh
+++ extract.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
-config_file=$1
+input_ver="$1"
+extract_dir="$2"
+
 extract_script=./extract.py
 
+# write config file.
+mkdir -p $extract_dir
+config_file=$extract_dir/extract_config.yaml
+python $extract_script write_config "$@" --config_file $config_file
+
 # generate task file from input arg_file 
-arg_file=`cat $config_file | shyaml get-value Raw.ARG_FILE`
-log_file=`cat $config_file | shyaml get-value Output.LOG`
+arg_file=`cat $config_file | shyaml get-value input_pas.arg_file`
 task_file=./extract.task
 python $extract_script print_task --arg_file $arg_file --config_file $config_file --task_file $task_file
+echo $task_file
 
 # extract eventChain data by gxpc.
-tmpDBDir=`cat $config_file | shyaml get-value Output.TMP_DB_DIR`
+log_file=`cat $config_file | shyaml get-value output.log`
+tmpDBDir=`cat $config_file | shyaml get-value output.tmp_db_dir`
 mkdir -p $tmpDBDir
 gxpc js -a work_file=extract.task -a cpu_factor=0.125 2> $log_file
 
